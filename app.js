b8b41759dc0e62c7054e6c5063dc28285eccd7fc
Subiyan <subiyanr813@gmail.com>
2025-09-07 13:02:41 +0530

    Final Commit : Changed name to see if the testing is working

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const name = "Your";
 
 app.get('/', (req, res) => {
-  res.send('Welcome My Page!');
+  const message = `Welcome ${name} Page!`;
+  res.send(message);
 })
 
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
 
-module.exports = {app, server}
+module.exports = {app, server, name}
--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
-const { app , server} = require('../app'); 
+const { app , server, name} = require('../app'); 
 
 describe('GET /', () => {
     it('should return Welcome My Page!',  async() => {
         const responce = await request(app).get('/');
         expect(responce.status).toBe(200);
-        expect(responce.text).toBe('Welcome My Page!');
+        expect(responce.text).toBe(`Welcome ${name} Page!`);
     })
 })
 
